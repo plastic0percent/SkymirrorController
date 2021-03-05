45fb5a7a0a91f6fdc9f13fb555f7561527f99517
Zhang Maiyun <myzhang1029@hotmail.com>
2021-03-05 13:45:51 +0800

    Add basic views

--- Skymirror Controller/ContentView.swift
+++ Skymirror Controller/ContentView.swift
@@ -7,10 +7,79 @@
 
 import SwiftUI
 
+class NumberInput: ObservableObject {
+    @Published var valueStr = "" {
+        didSet {
+            let filtered = valueStr.filter { $0.isNumber }
+            
+            if valueStr != filtered {
+                valueStr = filtered
+            }
+        }
+    }
+}
+
 struct ContentView: View {
+    @State private var fishRepellerOn = false
+    @State private var isFreqEditing = false
+    @ObservedObject var freqVal = NumberInput()
+    @State private var isMotorEditing = false
+    @State private var motorVal = 1500.0
+    @State private var isTurningEditing = false
+    @State private var turningVal = 38.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // Title
+        Text("Skymirror Controller").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+        
+        // Fish repeller toggle
+        Toggle(isOn: $fishRepellerOn) {
+            Text("Fish Repeller")
+        }
+        
+        if fishRepellerOn {
+            TextField(
+                "Frequency",
+                text: $freqVal.valueStr
+            ) { isEditing in
+                self.isFreqEditing = isEditing
+            } //onCommit: {
+            //validate(name: freqVal)
+            //}
+            .keyboardType(.decimalPad)
+            .disableAutocorrection(true)
+            .border(Color(UIColor.separator))
+            Text(freqVal.valueStr)
+                .foregroundColor(isFreqEditing ? .red : .blue)
+            
+        }
+        
+        // Motor control
+        VStack {
+            Text("Main Motor")
+            Slider(
+                value: $motorVal,
+                in: 1500...2000,
+                onEditingChanged: { editing in
+                    isMotorEditing = editing
+                }
+            )
+            Text("\(motorVal)")
+                .foregroundColor(isMotorEditing ? .red : .blue)
+        }
+        // Turning control
+        VStack {
+            Text("Direction")
+            Slider(
+                value: $turningVal,
+                in: 23...54,
+                onEditingChanged: { editing in
+                    isTurningEditing = editing
+                }
+            )
+            Text("\(turningVal)")
+                .foregroundColor(isTurningEditing ? .red : .blue)
+        }
     }
 }
 
