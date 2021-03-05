b625d19cbede7b2d91fe65fa843188ceaab33171
Zhang Maiyun <myzhang1029@hotmail.com>
2021-03-05 13:45:48 +0800

    Init XCode

---
+++ Skymirror Controller/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Skymirror Controller
+//
+//  Created by 张迈允 on 2021/3/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
