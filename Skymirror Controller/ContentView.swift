9178bfd6bfe3f5c5297cca4dc24f1a2d88515ba8
Zhang Maiyun <myzhang1029@hotmail.com>
2021-03-06 09:46:47 +0800

    Fix reading by changing to notification
    UI updated

--- Skymirror Controller/ContentView.swift
+++ Skymirror Controller/ContentView.swift
@@ -9,6 +9,11 @@ import SwiftUI
 import SwiftyBluetooth
 import CoreBluetooth
 
+enum DataError: Error{
+    case malformedDate(received: String)
+    case jsonError(message: String)
+}
+
 struct DataResponse: Decodable {
     let time: String
     let accel: [Float]
@@ -20,6 +25,70 @@ struct DataResponse: Decodable {
     let lon: Float
 }
 
+// Get a data value from the received payload
+func getData(key: String, payload: Data) throws -> String {
+    print(payload.base64EncodedString())
+    let decoded: DataResponse
+    do{
+        decoded = try JSONDecoder()
+            .decode(DataResponse.self, from: payload)
+    } catch DecodingError.dataCorrupted(let error) {
+        let description = error.underlyingError?.localizedDescription ?? "Unknown Error"
+        print("JSON Error: \(description)")
+        print("\(String(data: payload, encoding: .ascii)!)")
+        throw DataError.jsonError(message: description)
+    }
+    switch key {
+    case "time":
+        let timeStrUTC = decoded.time
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(abbreviation: "UTC")
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        let gotDate = formatter.date(from: timeStrUTC)
+        if gotDate == nil {
+            throw DataError.malformedDate(received: timeStrUTC)
+        }
+        formatter.timeZone = TimeZone.current
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return formatter.string(from: gotDate!)
+    case "accel":
+        return String(format: "%.2fx %.2fy %.2fz ms^-2",
+                      decoded.accel[0],
+                      decoded.accel[1],
+                      decoded.accel[2]
+        )
+    case "speed":
+        return String(format: "%.2fx %.2fy %.2fz ms^-1",
+                      decoded.speed[0],
+                      decoded.speed[1],
+                      decoded.speed[2]
+        )
+    case "displ":
+        return String(format: "%.2fx %.2fy %.2fz m",
+                      decoded.displ[0],
+                      decoded.displ[1],
+                      decoded.displ[2]
+        )
+    case "pressure":
+        return String(format: "%.2f kPa", decoded.pressure)
+    case "depth":
+        return String(format: "%.2f m", decoded.depth)
+    case "lat":
+        return "\(decoded.lat)"
+    case "lon":
+        return "\(decoded.lon)"
+    default:
+        return ""
+    }
+}
+
+// Get a human-readable name of the peripheral
+func getDeviceName(peripheral: Peripheral) -> String {
+    if peripheral.name != nil {
+        return peripheral.name!
+    }
+    return peripheral.identifier.uuidString
+}
 struct ContentView: View {
     // Whether the fish repeller beeper is on
     @State private var fishRepellerOn = false
@@ -34,7 +103,7 @@ struct ContentView: View {
     // Whether direction is being edited
     @State private var isTurningEditing = false
     // Current-set turing position
-    @State private var turningVal = 38.0
+    @State private var turningVal = 38.5
     // Whether an Alert relating to BLE is shown
     @State private var showBleAlert = false;
     // The Alert message
@@ -47,6 +116,10 @@ struct ContentView: View {
     // Whether the Scan pad is shown
     @State private var showScanPad = true;
     // Payload from the board
+    @State private var receivedPayload: Data = "".data(using: .ascii)!;
+    // Which payload is expected, 0 for status, 1 for image
+    @State private var expectedPayload: UInt8 = 0;
+    // Status payload, from receivedPayload
     @State private var statusPayload: Data = """
 {
 "time": "2021-01-05T16:00:00",
@@ -89,12 +162,40 @@ struct ContentView: View {
             case .success:
                 // Record the peripheral
                 usingPeripheral = peripheral
+                NotificationCenter.default.addObserver(forName: Peripheral.PeripheralCharacteristicValueUpdate,
+                                                       object: peripheral,
+                                                       queue: nil) { notification in
+                    let charac = notification.userInfo!["characteristic"] as! CBCharacteristic
+                    if let error = notification.userInfo?["error"] as? SBError {
+                        showBleAlert = true
+                        bleAlertMsg = error.localizedDescription
+                        return
+                    }
+                    if charac.isNotifying {
+                        let val = charac.value!
+                        receivedPayload.append(val)
+                        // Continue to process until EOT is received
+                        let eotpos = receivedPayload.firstIndex(of: 0x04)
+                        if eotpos != nil {
+                            if expectedPayload == 0 {
+                                // Update status payload, remove things after EOT
+                                statusPayload = receivedPayload[0..<eotpos!]
+                                print("Received data: \(statusPayload.base64EncodedString())")
+                                // Clear received payload
+                                receivedPayload.removeAll(keepingCapacity: true)
+                            }
+                        }
+                    }
+                }
+                
+                // We can then set a characteristic's notification value to true and start receiving updates to that characteristic
+                peripheral.setNotifyValue(toEnabled: true, forCharacWithUUID: "FFE1", ofServiceWithUUID: "FFE0") { result in
+                    // If there were no errors, you will now receive Notifications when that characteristic value gets updated.
+                    print("Notification: \(result)")
+                }
                 // Update the information every second
                 Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { timer in
-                    let tmpStatusPayload = request_info()
-                    if tmpStatusPayload != nil {
-                        statusPayload = tmpStatusPayload!
-                    }
+                    request_info()
                 })
                 return
             case .failure(let error):
@@ -129,30 +230,6 @@ struct ContentView: View {
         }
     }
     
-    // Read data from the FFE1 characteristc
-    func read() -> Data? {
-        if usingPeripheral == nil {
-            showBleAlert = true
-            bleAlertMsg = "You should connect first"
-            return nil
-        }
-        var value: Data?
-        usingPeripheral!.readValue(
-            ofCharacWithUUID: "FFE1",
-            fromServiceWithUUID: "FFE0"
-        ) { result in
-            switch result {
-            case .success(let data):
-                value = data
-            case .failure(let error):
-                showBleAlert = true
-                bleAlertMsg = error.localizedDescription
-                value = nil
-            }
-        }
-        return value
-    }
-    
     // Send calibrate signal
     func calibrate() {
         write(cmd: 0x00, arg: 0x00)
@@ -164,79 +241,18 @@ struct ContentView: View {
     }
     
     // Request information
-    func request_info() -> Data? {
+    func request_info() {
+        expectedPayload = 0;
         write(cmd: 0x02, arg: 0x00)
-        let value = read()
-        if value == nil {
-            showBleAlert = true
-            bleAlertMsg = "Cannot request information"
-        }
-        return value
     }
     
-    // Get a data value from the received payload
-    func getData(key: String, payload: Data) -> String {
-        let decoded: DataResponse
-        do {
-            decoded = try JSONDecoder().decode(DataResponse.self, from: payload)
-        } catch {
-            showBleAlert = true
-            bleAlertMsg = error.localizedDescription
-            return ""
-        }
-        switch key {
-        case "time":
-            let timeStrUTC = decoded.time
-            let formatter = DateFormatter()
-            formatter.timeZone = TimeZone(abbreviation: "UTC")
-            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
-            let gotDate = formatter.date(from: timeStrUTC)
-            if gotDate == nil {
-                showBleAlert = true
-                bleAlertMsg = "Malformed date"
-                return ""
-            }
-            formatter.timeZone = TimeZone.current
-            formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-            return formatter.string(from: gotDate!)
-        case "accel":
-            return String(format: "%.2fx %.2fy %.2fz ms^-2",
-                          decoded.accel[0],
-                          decoded.accel[1],
-                          decoded.accel[2]
-            )
-        case "speed":
-            return String(format: "%.2fx %.2fy %.2fz ms^-1",
-                          decoded.speed[0],
-                          decoded.speed[1],
-                          decoded.speed[2]
-            )
-        case "displ":
-            return String(format: "%.2fx %.2fy %.2fz m",
-                          decoded.displ[0],
-                          decoded.displ[1],
-                          decoded.displ[2]
-            )
-        case "pressure":
-            return String(format: "%.2f kPa", decoded.pressure)
-        case "depth":
-            return String(format: "%.2f m", decoded.depth)
-        case "lat":
-            return "\(decoded.lat)"
-        case "lon":
-            return "\(decoded.lon)"
-        default:
-            return ""
-        }
+    // Request image
+    func request_image() {
+        expectedPayload = 1;
+        write(cmd: 0x01, arg: 0x01)
     }
     
-    // Get a human-readable name of the peripheral
-    func getDeviceName(peripheral: Peripheral) -> String {
-        if peripheral.name != nil {
-            return peripheral.name!
-        }
-        return peripheral.identifier.uuidString
-    }
+    // MARK: View starts here
     
     var body: some View {
         // Title
@@ -281,6 +297,7 @@ struct ContentView: View {
             )
         }
         
+        // MARK: Data columns
         VStack {
             Divider()
             
@@ -302,89 +319,109 @@ struct ContentView: View {
                 LazyVGrid(columns: columns) {
                     ForEach(datas, id: \.1) {
                         let title = $0.0
-                        let value = getData(key: $0.1, payload: statusPayload)
+                        let value = try? getData(key: $0.1,
+                                                 payload: statusPayload
+                        )
                         // Data caption
                         Text("\(title)")
                             .bold()
                             .font(.system(size: 18))
                         // Data value
-                        Text("\(value)")
+                        Text("\(value ?? "Error")")
                             .font(.system(size: 16, weight: .light))
                     }
                 }
             }
         }
         
-        // Fish repeller control
+        // MARK: Fish repeller control
         VStack {
             Divider()
             // Fish repeller toggle
-            Toggle(isOn: $fishRepellerOn) {
-                Text("Fish Repeller")
+            HStack {
+                Spacer()
+                Toggle(isOn: $fishRepellerOn) {
+                    Text("Fish Repeller")
+                }
+                Spacer()
             }
             
             // Allow editing frequency only when on
             if fishRepellerOn {
                 Divider()
                 Text("Frequency")
-                Slider(
-                    value: $freqVal,
-                    in: 0...7679,
-                    onEditingChanged: { editing in
-                        isFreqEditing = editing
-                        if editing == false {
-                            // Commit value
-                            write(cmd: 0x40,
-                                  arg: UInt8(freqVal / 30))
+                HStack {
+                    Spacer()
+                    Slider(
+                        value: $freqVal,
+                        in: 0...7679,
+                        onEditingChanged: { editing in
+                            isFreqEditing = editing
+                            if editing == false {
+                                // Commit value
+                                write(cmd: 0x40,
+                                      arg: UInt8(freqVal / 30))
+                            }
                         }
-                    }
-                )
-                Text(String(format: "%.2f", freqVal))
-                    .foregroundColor(isFreqEditing ? .red : .blue)
+                    )
+                    Text(String(format: "%.2f", freqVal))
+                        .foregroundColor(isFreqEditing ? .red : .blue)
+                    Spacer()
+                }
             }
         }
         
-        // Motor control
+        // MARK: Motor control
         VStack {
             Divider()
             Text("Main Motor")
-            Slider(
-                value: $motorVal,
-                in: 1500...2000,
-                onEditingChanged: { editing in
-                    isMotorEditing = editing
-                    if editing == false {
-                        // Commit value
-                        write(cmd: 0x50,
-                              arg: UInt8(motorVal / 10))
+            HStack {
+                Spacer()
+                Slider(
+                    value: $motorVal,
+                    in: 1500...2000,
+                    onEditingChanged: { editing in
+                        isMotorEditing = editing
+                        if editing == false {
+                            // Commit value
+                            write(cmd: 0x50,
+                                  arg: UInt8(motorVal / 10))
+                        }
                     }
-                }
-            )
-            Text(String(format: "%.2f", motorVal))
-                .foregroundColor(isMotorEditing ? .red : .blue)
+                )
+                Text(String(format: "%.2f", motorVal))
+                    .foregroundColor(isMotorEditing ? .red : .blue)
+                Spacer()
+            }
         }
         
-        // Turning control
+        // MARK: Turning control
         VStack {
             Divider()
             Text("Direction")
-            Slider(
-                value: $turningVal,
-                in: 23...54,
-                onEditingChanged: { editing in
-                    isTurningEditing = editing
-                    if editing == false {
-                        // Commit value
-                        write(cmd: 0x60,
-                              arg: UInt8(turningVal))
+            HStack {
+                Spacer()
+                Text("L")
+                Slider(
+                    value: $turningVal,
+                    in: 23...54,
+                    onEditingChanged: { editing in
+                        isTurningEditing = editing
+                        if editing == false {
+                            // Commit value
+                            write(cmd: 0x60,
+                                  arg: UInt8(turningVal))
+                        }
                     }
-                }
-            )
+                )
+                Text("R")
+                Spacer()
+            }
             Text(String(format: "%.2f", turningVal))
                 .foregroundColor(isTurningEditing ? .red : .blue)
         }
         
-        // Calibration control
+        // MARK: Calibration control
         VStack {
             Divider()
             HStack {
@@ -399,7 +436,7 @@ struct ContentView: View {
             }
         }
         
-        // Footnote
+        // MARK: Footnote
         VStack {
             Divider()
             Text("Copyright \u{00a9} 2021, Plastic 0%. All rights reserved.")
